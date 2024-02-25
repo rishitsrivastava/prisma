16aab66e21e828c3e1e9cf87d512613d0df47d77
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-02-25 17:02:34 +0530

    update function with query

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,42 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function insertUser(username: string, password: string, firstName: string, lastName: string) {
-    prisma.user.create({
+    const res = await prisma.user.create({
         data: {
             email: username,
             password,
             firstName,
             lastName
+        },
+        select: {
+            id: true,
+            password: true,
         }
     })
+    console.log(res);
 }
+
+// insertUser("rishitsri@gmail.com", "123456", "Rishit", "Srivastava");
+
+interface UpdateParams{
+    firstName: string;
+    lastName: string;
+}
+
+async function updateUser(username: string, {
+    firstName,
+    lastName
+}: UpdateParams) {
+    const res = await prisma.user.update({
+        where: {email: username},
+        data: {
+            firstName,
+            lastName
+        }
+    })
+    console.log(res);
+}
+updateUser("rishitsri@gmail.com", {
+    firstName: "Rishi", 
+    lastName: "Srivastava"
+});
